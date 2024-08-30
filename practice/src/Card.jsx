2e82c10d7edd897503de12afe46da450545d30f4
chetan <chetan.sq05@gmail.com>
2024-08-30 17:36:04 +0530

    practice

--- practice/src/Card.jsx
+++ practice/src/Card.jsx
@@ -151,55 +151,92 @@ import Button from './Button';
 //     )
 // }
 
-const intialValue = "https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg";
+// const intialValue = "https://cdn.pixabay.com/photo/2024/05/26/10/15/bird-8788491_1280.jpg";
 
-const Card = () => {
+// const Card = () => {
 
-    const [defaultImage, setImage] = useState(intialValue);
-    const setImg = 'https://cdn.pixabay.com/photo/2024/02/26/19/39/monochrome-image-8598798_1280.jpg';
-    const setImages = () => {
-        setImage(setImg);
-    }
+//     const [defaultImage, setImage] = useState(intialValue);
+//     const setImg = 'https://cdn.pixabay.com/photo/2024/02/26/19/39/monochrome-image-8598798_1280.jpg';
+//     const setImages = () => {
+//         setImage(setImg);
+//     }
 
-    return (
-        <>
-            {/* second way for  */}
-            <div className="w-[300px] rounded-md border ">
-                <Button data={defaultImage} />
+//     return (
+//         <>
+//             {/* second way for  */}
+//             <div className="w-[300px] rounded-md border ">
+//                 <Button data={defaultImage} />
 
-                <div className="p-4">
-                    <h1 className="inline-flex items-center text-lg font-semibold">
-                        Bird &nbsp;
-                    </h1>
-                    <p className="mt-3 text-sm text-gray-600">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
-                    </p>
-                    <div className="mt-4">
-                        <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-                            #Macbook
-                        </span>
-                        <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-                            #Apple
-                        </span>
-                        <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-                            #Laptop
-                        </span>
-                    </div>
-                    <button
-                        onMouseEnter={setImages}
+//                 <div className="p-4">
+//                     <h1 className="inline-flex items-center text-lg font-semibold">
+//                         Bird &nbsp;
+//                     </h1>
+//                     <p className="mt-3 text-sm text-gray-600">
+//                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
+//                     </p>
+//                     <div className="mt-4">
+//                         <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
+//                             #Macbook
+//                         </span>
+//                         <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
+//                             #Apple
+//                         </span>
+//                         <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
+//                             #Laptop
+//                         </span>
+//                     </div>
+//                     <button
+//                         onMouseEnter={setImages}
 
-                        type="button"
-                        className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                    >
-                        Read
-                    </button>
+//                         type="button"
+//                         className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+//                     >
+//                         Read
+//                     </button>
 
-                </div>
-            </div>
-        </>
+//                 </div>
+//             </div>
+//         </>
 
-    )
-}
+//     )
+// }
+
+// const inImages = 'https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60'
+// const images = 'https://images.indianexpress.com/2024/01/coffee-freepik-1.jpg';
+// const Card = () => {
+//     const [img, setImgs] = useState(inImages);
+
+
+//     const setImg = () => {
+//         setImgs(images);
+//     }
+
+//     return (
+//         <div>
+//             <div className="w-[300px] rounded-md border">
+//                 <img
+//                     src={img}
+//                     alt="Laptop"
+//                     className="h-[200px] w-full rounded-md object-cover"
+
+//                 />
+//                 <div className="p-4">
+//                     <h1 className="text-lg font-semibold">About Macbook</h1>
+//                     <p className="mt-3 text-sm text-gray-600">
+//                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
+//                     </p>
+//                     <button onClick={setImg}
+//                         type="button"
+//                         className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+//                     >
+//                         Change Image
+//                     </button>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
 
 export default Card
 
