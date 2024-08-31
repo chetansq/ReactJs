c6e11401468181b624cae247a79a2523df5bd120
chetan <chetan.sq05@gmail.com>
2024-08-31 16:57:51 +0530

    routing complete

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
-
+// import Navbar from './components/Navbar'
+import Layout from './pages/layout'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+      <Layout />
     </>
   )
 }
--- routing/src/components/Navbar.jsx
+++ routing/src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import reactImg from '../assets/react.svg'
+import { Link } from 'react-router-dom'
+
 
 const Navbar = () => {
     return (
-        <div className=' flex bg-slate-200 items-center justify-between'>
+        <div className=' flex bg-slate-200 items-center justify-around p-4'>
             <div>
-                <img src={reactImg} alt="" className='h-20' />
+                <img src='https://reactrouter.com/_brand/react-router-stacked-color.png' alt="" className='h-20' />
             </div>
             <div>
                 <nav>
-                    <ul className='flex gap-6 '>
-                        <li><a href="">Home</a></li>
-                        <li><a href="">About</a></li>
-                        <li><a href="">Cart</a></li>
-                        <li><a href="">Shop</a></li>
-                        <li><a href="">Contact</a></li>
-                        <li><a href="">Login</a></li>
+                    <ul className='flex gap-8 '>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/cart">Cart</Link></li>
+                        <li><Link to="/shop">Shop</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                        <li><Link to="/login">Login</Link></li>
                     </ul>
                 </nav>
             </div>
