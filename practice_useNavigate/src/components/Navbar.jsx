fc6b72abcbbdb1dc127bf7da52ebcd3799ff6f1c
chetan <chetan.sq05@gmail.com>
2024-09-02 17:47:31 +0530

    practice of useNavigate

---
+++ practice_useNavigate/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+import { NavLink, Outlet } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <div>
+
+            <div className='bg-slate-200 flex justify-around p-4 items-center'>
+                <div>
+                    <img src="https://reactrouter.com/_brand/react-router-stacked-color.png" alt="" className='h-20' />
+                </div>
+                <div>
+                    <nav>
+                        <ul className='flex gap-8'>
+                            <li><NavLink to='/'>Home</NavLink></li>
+                            <li><NavLink to='/about'>About</NavLink></li>
+                            <li><NavLink to='/cart'>Cart</NavLink></li>
+                            <li><NavLink to='/shop'>Shop</NavLink></li>
+                            <li><NavLink to='/contact'>Contact</NavLink></li>
+                            <li><NavLink to='/login'>Login</NavLink></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <Outlet />
+        </div>
+    )
+}
+
+export default Navbar
---
+++ practice_useNavigate/src/pages/Shop.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import RouterHooks from './routerHooks'
+
+const Shop = () => {
+  return (
+    <div>
+
+      <div className='text-center p-32 bg-slate-300 mt-32'>
+        <h1>This is <span>Shop</span> Page</h1>
+      </div>
+      <RouterHooks />
+    </div>
+  )
+}
+
+export default Shop
