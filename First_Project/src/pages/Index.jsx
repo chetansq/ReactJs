7f772c0c589c53d5eeee4417f20d6d0cb3c437b3
Anaghan007 <anaghan245@gmail.com>
2024-09-09 10:32:48 +0530

    marquee changes 09-09-24

--- First_Project/src/pages/Index.jsx
+++ First_Project/src/pages/Index.jsx
@@ -72,8 +72,8 @@ export const Sliders = () => {
                 {/* Slider Section End */}
 
                 {/* Marquee Section Start */}
-                <div className='bg-yellow-100 flex text-nowrap overflow-x-hidden marqueeContainer'>
-                    <div className='py-5 flex marqueeDiv'>
+                <div className='bg-yellow-200 flex text-nowrap overflow-x-hidden marqueeContainer lg:py-3'>
+                    <div className='py-2 flex marqueeDiv max-md:py-3 max-md:text-lg'>
                         <p className='flex gap-2 items-center text-xl px-4'> <HiBolt /> <span> Spring Clearance Event: Save Up to 70% </span></p>
                         <p className='flex gap-2 items-center text-xl px-4'> <HiBolt /> <span> Spring Clearance Event: Save Up to 70% </span></p>
                         <p className='flex gap-2 items-center text-xl px-4'> <HiBolt /> <span> Spring Clearance Event: Save Up to 70% </span></p>
@@ -83,7 +83,7 @@ export const Sliders = () => {
                         <p className='flex gap-2 items-center text-xl px-4'> <HiBolt /> <span> Spring Clearance Event: Save Up to 70% </span></p>
 
                     </div>
-                    <div className='py-5 flex marqueeDiv'>
+                    <div className='py-5 flex marqueeDiv max-md:py-3 max-md:text-lg'>
                         <p className='flex gap-2 items-center text-xl px-4'> <HiBolt /> <span> Spring Clearance Event: Save Up to 70% </span></p>
                         <p className='flex gap-2 items-center text-xl px-4'> <HiBolt /> <span> Spring Clearance Event: Save Up to 70% </span></p>
                         <p className='flex gap-2 items-center text-xl px-4'> <HiBolt /> <span> Spring Clearance Event: Save Up to 70% </span></p>
@@ -129,7 +129,7 @@ function HeroSlide({ details }) {
                         <p className='text-4xl md:text-5xl lg:text-[80px]'>{details.tittle[0]} <br /> {details.tittle[1]}</p>
                         <p className='md:text-xl my-4 mb-6 max-md:hidden'>{details.description}</p>
 
-                        <a href="" className="bg-black w-fit text-white text-md font-semibold rounded md:px-7 px-4 py-2 md:py-3 mt-4 flex items-center gap-1 relative before:absolute before:content-[''] before:BgGradientMove before:h-full before:w-full before:translate-x-[100%]   hover:before:-translate-x-[100%] before:duration-1000 overflow-hidden z-0 before:-skew-x-12 hover:before:BgGradient">
+                        <a href="" className="bg-black w-fit text-white text-md font-semibold rounded md:px-7 px-4 py-2 md:py-3 mt-4 flex items-center gap-1 relative before:absolute before:content-[''] before:BgGradientMove before:h-full before:w-full before:translate-x-[100%] hover:before:-translate-x-[150%] before:duration-1000 overflow-hidden z-0 before:-skew-x-12 hover:before:BgGradient">
                             <span>Shop Collection</span>
                             <IoIosArrowForward className='text-white font-bold text-lg' />
                         </a>
