d2ac38416f731d5fddf426243cfc6f29ab6033a9
Chetan <Chetan.sq05@gmail.com>
2024-09-11 11:40:37 +0530

    changes

---
+++ task usecontext/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './App.css'
+import { createContext } from 'react'
+import Layout from './Components/Layout'
+
+export const data = createContext('skillqode')
+
+// const obj = {
+//   fullname: 'arihant',
+//   age: 20
+// }
+
+function cards() {
+
+  console.log("this is card")
+  return (
+    <>
+      <p>
+        this is card function
+      </p>
+    </>
+  )
+
+}
+
+const App = ({ children }) => {
+  return (
+    <div>
+      <data.Provider value={cards()}>
+        <Layout >
+          {children}
+          <h1 className='text-white bg-red-700'>this is layout page</h1>
+        </Layout>
+      </data.Provider>
+    </div>
+  )
+}
+
+export default App
