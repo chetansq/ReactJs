238305b9583f658224ba14d7b45cf209165d860c
Chetan <Chetan.sq05@gmail.com>
2024-09-21 10:39:39 +0530

    fetch api data another

--- task api recipe/src/App.jsx
+++ task api recipe/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css'
 import Recipe_api from './Recipe_api'
+import Recipes from './Recipes'
 
 
 const App = () => {
   return (
     <div>
-      <Recipe_api />
-
+      {/* <Recipe_api /> */}
+      <Recipes />
     </div>
   )
 }
