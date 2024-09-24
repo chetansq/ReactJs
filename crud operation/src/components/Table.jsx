700a989c4b6f879b5a3525e7df7b9b73dc489c9c
chetan <chetan.sq05@gmail.com>
2024-09-24 18:02:22 +0530

    crud operation complete 24-09-24

--- crud operation/src/components/Table.jsx
+++ crud operation/src/components/Table.jsx
@@ -3,6 +3,9 @@ import { FiSearch } from "react-icons/fi";
 import { TiDelete } from "react-icons/ti";
 import { MdOutlineDownloadForOffline } from "react-icons/md";
 import { TiPlus } from "react-icons/ti";
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+
 
 const tableDetails = [
     {
@@ -45,7 +48,7 @@ const tableDetails = [
 const Table = () => {
 
     return (
-        <div>
+        <div className='mx-5'>
             <p className='text-4xl my-5'>Crud Operation</p>
 
             <hr />
@@ -54,57 +57,92 @@ const Table = () => {
 
                 <div className='flex items-center  w-[20%]'>
                     <input type="text" placeholder='Type a name...' className='border  p-1  w-full' />
-                    <div className='bg-sky-700 p-2 text-white rounded-e-md'>
+                    <div className='bg-sky-700 p-2 text-white rounded-e-md hover:bg-white border-2 border-sky-700 hover:text-sky-700'>
                         <FiSearch className='' />
                     </div>
                 </div>
 
                 <div className='flex gap-3  items-center'>
-                    <button className='flex  items-center bg-red-500 text-white p-2 rounded-md'><TiDelete className='text-xl' /> Delete Selected</button>
-                    <button className='flex  items-center bg-cyan-600 text-white p-2 rounded-md'><MdOutlineDownloadForOffline className='text-xl' /> Export CSV</button>
-                    <button className='flex  items-center bg-cyan-700  text-white p-2 rounded-md'> <TiPlus className='text-xl' />Create Record</button>
+
+                    <button className='flex  items-center bg-red-500 text-white p-2 rounded-md  hover:bg-white border-2 border-red-500 hover:text-red-500'>
+                        <TiDelete className='text-xl' />
+                        Delete Selected
+                    </button>
+
+                    <button className='flex  items-center bg-cyan-600 text-white p-2 rounded-md hover:bg-white border-2 border-cyan-600 hover:text-cyan-600'>
+                        <MdOutlineDownloadForOffline className='text-xl' />
+                        Export CSV
+                    </button>
+
+                    <button className='flex  items-center bg-cyan-700  text-white p-2 rounded-md
+                    hover:bg-white border-2 border-cyan-700 hover:text-cyan-700'> <TiPlus className='text-xl' />
+                        Create Record
+                    </button>
                 </div>
+
             </div>
+
 
             <div>
-                <div>
-                    <table className="table-auto w-full border-2">
-                        <thead>
-                            <tr className='py-5'>
-                                <th><input type="checkbox" /></th>
-                                <th>Name</th>
-                                <th>Description</th>
-                                <th>Price</th>
-                                <th>Category</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody className=''>
+                <table className="table-auto w-full " >
+                    <thead className='border-2 '>
+                        <tr className='text-xl'>
+                            <th><input type="checkbox" /></th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Price</th>
+                            <th>Category</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody className=''>
 
-                            {
-                                tableDetails.map((current) => {
-                                    return (
-                                        <tr className='text-center '>
-                                            <td><input type="checkbox" /></td>
-                                            <td> {current.name} </td>
-                                            <td> {current.desc} </td>
-                                            <td> {current.price} </td>
-                                            <td> {current.category} </td>
-                                            <td className='flex justify-center gap-4'>
-                                                <button className='bg-cyan-500 py-1 px-4 rounded-md text-white '>Edit</button>
-                                                <button className='bg-red-500 py-1 px-4 rounded-md text-white'> Delete</button>
-                                            </td>
-                                        </tr>
+                        {
+                            tableDetails.map((current, index) => {
+                                return (
+                                    <tr className='text-center border-0 ' key={index}>
+                                        <td><input type="checkbox" /></td>
+                                        <td> {current.name} </td>
+                                        <td> {current.desc} </td>
+                                        <td> {current.price} </td>
+                                        <td> {current.category} </td>
+                                        <td className='flex justify-center gap-4 border'>
+                                            <button className='bg-cyan-500 py-1 px-4 rounded-md text-white border-2 border-cyan-500 hover:bg-white hover:text-cyan-700 '>Edit</button>
+                                            <button className='bg-red-500 py-1 px-4 rounded-md text-white hover:text-red-500 hover:bg-white border-2 border-red-500'> Delete</button>
+                                        </td>
+                                    </tr>
 
-                                    )
-                                })
-                            }
+                                )
+                            })
+                        }
 
-                        </tbody>
-                    </table>
+                    </tbody>
+                </table>
+            </div>
+
+            <div className='flex justify-between items-center my-8 '>
+
+                <div className='flex items-center'>
+                    <a href='' className='bg-cyan-800 text-white p-2 text-xl hover:text-cyan-800 border-2 border-cyan-800 hover:bg-white'>
+                        <MdKeyboardDoubleArrowLeft />
+                    </a>
+                    <a href="" className='px-2 py-1.5 bg-gray-100'>1</a>
+                    <a href="" className='px-2 py-1.5 bg-gray-100'>2</a>
+                    <a href="" className='px-2 py-1.5 bg-gray-100'>...</a>
+                    <a href='' className='bg-cyan-800 text-white p-2 text-xl  hover:text-cyan-800 border-2 border-cyan-800 hover:bg-white'>
+                        <MdKeyboardDoubleArrowRight />
+                    </a>
+                </div>
+
+                <div className='flex items-center  w-[20%]'>
+                    <input type="text" placeholder='Type a name...' className='border  p-1.5  w-full' />
+                    <div className='bg-sky-700 p-2 px-2 text-white rounded-e-md'>
+                        GO
+                    </div>
                 </div>
 
             </div>
+
 
 
         </div>
