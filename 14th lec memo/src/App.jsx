896f87bc1d095c4f111c278494c119d31c6231ce
Chetan <Chetan.sq05@gmail.com>
2024-09-24 10:18:34 +0530

    changes

---
+++ 14th lec memo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Memo from './Hooks/Memo'
+
+const App = () => {
+  return (
+    <div>
+      <Memo />
+    </div>
+  )
+}
+
+export default App
