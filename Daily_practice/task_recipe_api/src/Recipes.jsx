1839a3e45f9a630fcb83faffb713a3fe8f653edd
chetan <chetan.sq05@gmail.com>
2024-09-25 16:21:47 +0530

    changes

--- Daily_practice/task_recipe_api/src/Recipes.jsx
+++ Daily_practice/task_recipe_api/src/Recipes.jsx
@@ -3,25 +3,29 @@ import React, { useEffect, useState } from 'react'
 const Recipes = () => {
 
     const [data, setData] = useState([]);
-    useEffect(
+    useEffect(() => {
+
 
         fetch('https://dummyjson.com/recipes')
             .then(res => res.json())
-            .then(res => res.recipes);
-    setData(res.recipes)
-    )
-return (
-    <div>
-        {
-            data.map((crr) => {
-                <div className="">
-                    <p>{crr.name}</p>
-                    <img src={crr.image} alt="" />
-                </div>
+            .then((res) => {
+                setData(res.recipes)
+
             })
-        }
-    </div>
-)
+    }, []
+    )
+    return (
+        <div>
+            {
+                data.map((crr) => {
+                    <div className="">
+                        <p>{crr.name}</p>
+                        <img src={crr.image} alt="" />
+                    </div>
+                })
+            }
+        </div>
+    )
 }
 
 export default Recipes
