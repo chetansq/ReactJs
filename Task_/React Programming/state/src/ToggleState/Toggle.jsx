377bc7aaed614556ebdbaafea83b47647c8a47b8
chetan <chetan.sq05@gmail.com>
2024-10-03 19:13:21 +0530

    task

---
+++ Task_/React Programming/state/src/ToggleState/Toggle.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Card'
+
+const Toggle = () => {
+
+
+    return (
+        <div>
+            <Card />
+        </div>
+    )
+}
+
+export default Toggle
