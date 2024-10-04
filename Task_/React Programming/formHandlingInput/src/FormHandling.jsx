20b0bd2a4b6646258d785c77be1cc1d04b5a6240
chetan <chetan.sq05@gmail.com>
2024-10-04 18:03:38 +0530

    task form input handling 10-04-24

--- Task_/React Programming/formHandlingInput/src/FormHandling.jsx
+++ Task_/React Programming/formHandlingInput/src/FormHandling.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FormHandling = () => {
 
+    const [data, setData] = useState([]);
+
+    const obj = () => {
+        const user = document.getElementById('user_name').value;
+        const pass_word = document.getElementById('pwd').value;
+        setData([user, pass_word]);
+
+    }
+
     return (
-        <div className='flex  justify-center items-center h-screen bg-slate-400'>
-            <form action="" className='flex flex-col justify-center items-center  gap-4 p-5'>
+        <div className='flex flex-col gap-6 justify-center items-center h-screen bg-slate-400 '>
+            <form action="" onSubmit={obj} className='flex flex-col justify-center items-center w-[30%] p-20 gap-4 rounded bg-gray-700 '>
                 <label htmlFor="username"></label>
-                <input type="text" placeholder='UserName' className='border p-2 px-5 rounded' />
+                <input type="text" placeholder='UserName' className='border p-2 px-5 rounded w-full' id='user_name' />
 
                 <label htmlFor="password"></label>
+                <input type="password" placeholder='Password' className='border p-2 px-5 rounded w-full' id='pwd' />
 
-
-                <input type="password" placeholder='password' className='border p-2 px-5 rounded' />
-
-
-                <button type='submit' className='bg-blue-500 p-2'>Submit</button>
+                <button type='submit' className='bg-black text-white p-2 hover:bg-gray-300 hover:text-black rounded' >Submit</button>
             </form>
+
+            <div className='flex flex-col gap-4'>
+                <p>your Name:{data[0]}</p>
+                <p>password:{data[1]}</p>
+
+            </div>
         </div>
     )
 }
