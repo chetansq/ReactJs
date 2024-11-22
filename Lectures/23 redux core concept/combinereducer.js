c48c2dc15d8f3499084ad3f4ab6103521b38d59c
Chetan <Chetan.sq05@gmail.com>
2024-11-22 11:29:23 +0530

    combine reducer 22-11-24

---
+++ Lectures/23 redux core concept/combinereducer.js
@@ -0,0 +1,97 @@
+const redux = require('redux')
+const createStore = redux.createStore
+const  combineReducers  = redux.combineReducers
+const  applyMiddleware= redux.applyMiddleware
+const reduxLogger = require('redux-logger')
+const logger = reduxLogger.createLogger()
+
+// constant
+
+const BUY_CHOCOLATE = 'BUY_CHOCOLATE'
+const BUY_COLDRINKS = 'BUY_COLDRINKS'
+const BUY_PIZZA = 'BUY_PIZZA'
+
+// Redux Action
+
+function buy_chocolate() {
+    return {
+        type: BUY_CHOCOLATE,
+        info: "first redux action"
+    }
+}
+
+function buy_coldrinks() {
+    return {
+        type: BUY_COLDRINKS,
+        info: "second redux action"
+    }
+}
+
+function buy_pizza() {
+    return {
+        type: BUY_PIZZA,
+        info: "third redux action"
+    }
+}
+
+// initialstate for redux
+
+const initialstate = {
+    NumOfChocolate: 10,
+    NumOfColdrinks: 15,
+    NumOfPizza: 5
+
+}
+
+// Redux Reducer
+
+const ChocoReducer = (state = initialstate, action) => {
+    switch (action.type) {
+        case BUY_CHOCOLATE: return {
+            ...state,
+            NumOfChocolate: state.NumOfChocolate - 1
+        }
+
+        default: return state
+    }
+}
+const CakeReducer = (state = initialstate, action) => {
+    switch (action.type) {
+
+        case BUY_COLDRINKS: return {
+            ...state,
+            NumOfColdrinks: state.NumOfColdrinks - 1
+        }
+
+        default: return state
+    }
+}
+const PizzaReducer = (state = initialstate, action) => {
+    switch (action.type) {
+
+        case BUY_PIZZA: return {
+            ...state,
+            NumOfPizza: state.NumOfPizza - 1
+        }
+
+        default: return state
+    }
+}
+
+// redux store
+
+const rootReducer = combineReducers({
+    ChocoReducer, CakeReducer, PizzaReducer
+})
+
+const store = createStore(rootReducer, applyMiddleware(logger))
+
+console.log(store.getState())
+
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(buy_chocolate())
+store.dispatch(buy_coldrinks())
+store.dispatch(buy_pizza())
+
+unsubscribe()
