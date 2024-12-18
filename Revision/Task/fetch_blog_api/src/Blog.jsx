3544eec24d19a0ca5663da6a4f39795aca5d9b85
Chetan <Chetan.sq05@gmail.com>
2024-12-18 11:43:34 +0530

    change 18-12-24

--- Revision/Task/fetch_blog_api/src/Blog.jsx
+++ Revision/Task/fetch_blog_api/src/Blog.jsx
@@ -1,33 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
 import 'axios'
 import axios from 'axios'
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 // https://dummyapi.online/api/blogposts
 
-const Blog = () => {
-    return (
-        <div>
-            <a
-                href="#"
-                className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
-            >
-                <img
-                    className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                    src="/docs/images/blog/image-4.jpg"
-                    alt=""
-                />
-                <div className="flex flex-col justify-between p-4 leading-normal">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Noteworthy technology acquisitions 2021
-                    </h5>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in
-                        reverse chronological order.
-                    </p>
-                </div>
-            </a>
 
+
+const Blog = () => {
+
+    const [Data, setData] = useState([])
+
+    useEffect(() => {
+
+        axios.get('https://dummyapi.online/api/blogposts').then(res => {
+            setData(res.data)
+            console.log(res.data)
+        }
+        ).catch((error) => error);
+    }, [])
+
+    return (
+        <div className=' mt-10'>
+            {
+                Data.map((details) => {
+                    return (
+
+                        <div key={details.id} className='flex justify-center border max-w-sm bg-white rounded-lg shadow '>
+
+                            {/* <img
+                                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                                src={details.image}
+                                alt=""
+                            /> */}
+                            <div className="flex flex-col justify-between p-4 leading-normal">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-black ">
+                                    {details.title}
+                                </h5>
+                                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                    {details.content
+                                    }
+                                </p>
+
+                                <div className='flex justify-between'>
+                                    <p className='flex flex-col text-left'>Author
+                                        <span className='text-slate-700'>
+                                            {details.author}
+                                        </span>
+                                    </p>
+                                    <p className='flex flex-col text-right'> Date
+                                        <span className='text-slate-700'>
+                                            {details.date_published}
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    )
+                }
+                )
+            }
+
+
+            <Stack spacing={10}>
+                <Pagination count={50} variant="outlined" shape="rounded" color='primary' />
+            </Stack>
         </div>
     )
 }
