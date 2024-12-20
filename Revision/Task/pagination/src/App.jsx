46df29080be2ebb550807e8280f0779219baac54
Dhaval Solanki <dhavalas24@gmail.com>
2024-12-20 19:02:28 +0530

    changes

---
+++ Revision/Task/pagination/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Blog from './Blog'
+
+function App() {
+
+  return (
+    <>
+      <Blog />
+    </>
+  )
+}
+
+export default App
---
+++ Revision/Task/pagination/src/Blog.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+
+
+// https://dummyapi.online/api/blogposts
+
+
+
+const Blog = () => {
+
+    const [Data, setData] = useState([])
+    const [index, setIndex] = useState(1)
+
+
+
+    const production_data = 6;
+
+    // pagination
+
+    let last_index = index * production_data;
+
+    let first_index = last_index - production_data;
+
+    let product_data_list = Data.slice(first_index, last_index);
+
+
+    console.log('array from ', Array.from(product_data_list));
+
+
+    async function pagination() {
+        axios.get('https://dummyapi.online/api/blogposts')
+            .then(res => {
+                setData(res.data)
+            }
+            ).catch((error) => error);
+    }
+    useEffect(() => {
+        pagination()
+    }, [])
+
+    // const List = [Data[index]];
+    // Data[index]
+
+    // console.log('data index of', Data[index]);
+
+    // let abc = Array.from(product_data_list)
+
+
+    // const nextState = () => {
+    //     if (last_index >= Data.length) {
+    //         setIndex(0)
+    //     }
+    //     else {
+    //         setIndex((prev) => prev + 1)
+    //         console.log('index + 1', index);
+
+    //     }
+    // }
+    // const prevState = () => {
+    //     if (index == 0) {
+    //         setIndex(0)
+    //     }
+    //     else {
+    //         setIndex(index - 1)
+    //         console.log(index);
+
+    //     }
+    // }
+
+
+    return (
+        <div className='  mt-10'>
+
+            <div className='flex flex-wrap  my-10 items-center justify-center  gap-7' >
+
+                {
+                    product_data_list.map((details) => {
+                        return (
+
+                            <div key={details.id} className='flex justify-center border max-w-sm bg-white rounded-lg shadow '>
+
+                                {/* <img
+                                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                                src={details.image}
+                                alt=""
+                            /> */}
+                                <div className="flex h-[600px]  flex-col justify-between p-4 leading-normal">
+                                    <p className='bg-black text-white w-fit p-2 border-e-4 border-b-4 border-slate-400 shadow'>{details.id}</p>
+
+                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-black ">
+                                        {details.title}
+                                    </h5>
+                                    <hr className='my-2' />
+                                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                        {details.content
+                                        }
+                                    </p>
+
+                                    <div className='flex justify-between'>
+                                        <p className='flex flex-col text-left'>Author
+                                            <span className='text-slate-700'>
+                                                {details.author}
+                                            </span>
+                                        </p>
+                                        <p className='flex flex-col text-right'> Date
+                                            <span className='text-slate-700'>
+                                                {details.date_published}
+                                            </span>
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        )
+                    }
+                    )
+                }
+            </div>
+            <hr />
+            <div className='flex justify-between  gap-8 my-2'>
+                <button className='btn' onClick={() => setIndex((prev) => prev - 1)} disabled={first_index <= 0}>Previous</button>
+                <span className='bg-red-300 p-2'>{index} </span>
+                <button className='btn' onClick={() => setIndex((next) => next + 1)} disabled={last_index >= Data.length}>Next</button>
+            </div>
+        </div>
+    )
+}
+
+export default Blog
+
