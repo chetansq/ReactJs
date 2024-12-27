7bae86050eeb73c827d25625f223405158a5a9c1
Dhaval Solanki <dhavalas24@gamil.com>
2024-12-27 17:52:22 +0530

    changes

--- Exam/fetch_api/src/Fetch_api.jsx
+++ Exam/fetch_api/src/Fetch_api.jsx
@@ -26,65 +26,68 @@ const Fetch_api = () => {
                 {
                     Data.map((item) => {
                         return (
-                            <div key={item.id} className='hover:dark:bg-black hover:dark:text-white p-2 flex  flex-col  gap-6 bg-slate-100 border border-black shadow-xl'>
-                                <div className='p-2 flex  flex-col items-center justify-center gap-6'>
-                                    <p className='text-2xl line-clamp-1 text-justify text-slate-700 font-semibold  bg-slate-200 p-2 '>
+                            <div key={item.id} className='group/card '>
+                                <div  className='group/card  group-hover/card:dark:bg-black p-2 flex  flex-col  gap-6 bg-slate-100 border border-black shadow-xl'>
 
-                                        {item.title}
-                                    </p>
+                                    <div className='p-2 flex  flex-col items-center justify-center gap-6'>
+                                        <p className='text-2xl line-clamp-1 text-justify text-slate-700 group-hover/card:dark:text-white group-hover/card:dark:bg-transparent group-hover/card:dark:shadow-white group-hover/card:dark:shadow-sm font-semibold  bg-slate-200 p-2 '>
 
-                                    <p className='line-clamp-4 text-justify text-gray-800 hover:dark:text-white '>
-                                        {item.body}
-                                    </p>
-                                </div>
-                                <div className='flex gap-2 font-semibold'>
-                                    Tags :
-                                    <div className='flex gap-2 text-blue-800'>
-                                        <p>
-                                            {item.tags[0]}
+                                            {item.title}
                                         </p>
-                                        |            
-                                        <p>
-                                            {item.tags[1]}
-                                        </p>
-                                        |
-                                        <p>
-                                            {item.tags[2]}
+
+                                        <p className='line-clamp-4 text-justify text-gray-800 group-hover/card:dark:text-white '>
+                                            {item.body}
                                         </p>
                                     </div>
+                                    <div className='flex gap-2 font-semibold group-hover/card:dark:text-white'>
+                                        Tags :
+                                        <div className='flex gap-2 text-blue-800 group-hover/card:dark:text-blue-300'>
+                                            <p>
+                                                {item.tags[0]}
+                                            </p>
+                                            |
+                                            <p>
+                                                {item.tags[1]}
+                                            </p>
+                                            |
+                                            <p>
+                                                {item.tags[2]}
+                                            </p>
+                                        </div>
 
-                                </div>
-                                <div className='flex justify-between gap-4'>
-                                    <div className='flex items-center gap-2'>
-                                        <p className='flex items-center gap-1'>
-                                            <BiLike className='text-blue-800 ' />
-                                            <samp>
-                                                {item.reactions.likes}
+                                    </div>
+                                    <div className='flex justify-between gap-4'>
+                                        <div className='flex items-center gap-2'>
+                                            <p className='flex items-center gap-1'>
+                                                <BiLike className='text-blue-800 group-hover/card:dark:text-blue-300 ' />
+                                                <samp className='group-hover/card:dark:text-white'>
+                                                    {item.reactions.likes}
+                                                </samp>
+                                            </p>
+                                            |
+                                            <p className='flex items-center gap-1'>
+                                                <BiDislike className='text-slate-700' />
+                                                <span className='group-hover/card:dark:text-white'>
+                                                    {item.reactions.dislikes}
+                                                </span>
+
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className='flex justify-between gap-2'>
+                                        <p>
+                                            <span className='font-semibold group-hover/card:dark:text-white'> Views :</span>
+                                            <samp className='px-2 group-hover/card:dark:text-white'>
+                                                {item.views}
                                             </samp>
                                         </p>
-                                        |
-                                        <p className='flex items-center gap-1'>
-                                            <BiDislike className='text-slate-700' />
-                                            <span>
-                                                {item.reactions.dislikes}
+                                        <p>
+                                            <span className='font-semibold group-hover/card:dark:text-white'>UserId :</span>
+                                            <span className='px-2 group-hover/card:dark:text-white'>
+                                                {item.userId}
                                             </span>
-
                                         </p>
                                     </div>
-                                </div>
-                                <div className='flex justify-between gap-2'>
-                                    <p>
-                                       <span className='font-semibold'> Views :</span>
-                                        <samp className='px-2'>
-                                            {item.views}
-                                        </samp>
-                                    </p>
-                                    <p>
-                                        <span className='font-semibold'>UserId :</span>
-                                        <span className='px-2'>
-                                            {item.userId}
-                                        </span>
-                                    </p>
                                 </div>
 
 
