8a1718609497b40c8f9027393583c65f6ccc666d
Dhaval Solanki <dhavalas24@gamil.com>
2025-01-07 18:15:44 +0530

    changes

--- Revision/Task_/E_library_management_system/src/Pages/BookCard.jsx
+++ Revision/Task_/E_library_management_system/src/Pages/BookCard.jsx
@@ -15,37 +15,43 @@ const BookCard = () => {
     return (
 
         <div className='flex  justify-center items-center'>
+            {
+                booklist.map((details) => {
+                    return (
+                        <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8  " key={details}>
 
-            <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8  ">
+                            <div className="flow-root">
+                                <ul role="list" className="divide-y divide-gray-200">
 
-                <div className="flow-root">
-                    <ul role="list" className="divide-y divide-gray-200">
+                                    <li className="pt-3 pb-0 sm:pt-4">
+                                        <div className="flex items-center ">
 
-                        <li className="pt-3 pb-0 sm:pt-4">
-                            <div className="flex items-center ">
+                                            <div className="flex-1 min-w-0 ms-4">
+                                                <p className="text-sm font-medium text-gray-900 truncate ">
+                                                    Title : {details.title}
+                                                </p>
+                                                <p className="text-sm text-gray-500 truncate ">
+                                                    Author :  {details.author}
+                                                </p>
+                                                <p className="text-sm text-gray-500 truncate ">
+                                                    Status:{details.status}
+                                                </p>
+                                            </div>
+                                            <div className="inline-flex items-center text-base font-semibold text-gray-900  gap-4">
+                                                <button className='bg-green-600 text-white rounded hover:bg-green-100 hover:text-green-700 border hover:border-green-700 py-1 px-3'>Update</button>
+                                                <button className='bg-red-600 text-white rounded hover:bg-red-100 hover:text-red-700 border hover:border-red-700 py-1 px-3'>Delete</button>
+                                            </div>
 
-                                <div className="flex-1 min-w-0 ms-4">
-                                    <p className="text-sm font-medium text-gray-900 truncate ">
-                                        Title : {booklist.title}
-                                    </p>
-                                    <p className="text-sm text-gray-500 truncate ">
-                                        Author :  {booklist.author}
-                                    </p>
-                                    <p className="text-sm text-gray-500 truncate ">
-                                        {booklist.status}
-                                    </p>
-                                </div>
-                                <div className="inline-flex items-center text-base font-semibold text-gray-900  gap-4">
-                                    <button className='bg-green-600 text-white rounded hover:bg-green-100 hover:text-green-700 border hover:border-green-700 py-1 px-3'>Update</button>
-                                    <button className='bg-red-600 text-white rounded hover:bg-red-100 hover:text-red-700 border hover:border-red-700 py-1 px-3'>Delete</button>
-                                </div>
-
+                                        </div>
+                                    </li>
+                                </ul>
                             </div>
-                        </li>
-                    </ul>
-                </div>
 
-            </div>
+                        </div>
+                    )
+                })
+            }
+
 
         </div>
     )
