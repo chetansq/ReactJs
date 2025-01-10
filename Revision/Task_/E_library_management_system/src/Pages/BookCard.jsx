26b0afa03a1123050e44b165e8e978322220953d
Chetan <Chetan.sq05@gmail.com>
2025-01-10 10:55:09 +0530

    changes

--- Revision/Task_/E_library_management_system/src/Pages/BookCard.jsx
+++ Revision/Task_/E_library_management_system/src/Pages/BookCard.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import Book_Context from '../context/Bookcontext'
 import { useNavigate } from 'react-router-dom'
+import BookDetails from './BookDetails'
 
 const BookCard = () => {
 
@@ -9,41 +10,22 @@ const BookCard = () => {
     const [search, setSearch] = useState("")
     const { booklist } = useContext(Book_Context)
 
-    
-
     console.log('book list', booklist);
 
     const { setBook } = useContext(Book_Context)
 
-    // const search_data = () => {
-    //     let input_ = document.getElementById('search').value.toLowerCase();
-    //     setSearch(input_);
+    // const set_BookDetails = () => {
+
     // }
-
-    const set_BookDetails = () => {
-
-        setBook(booklist.filter((item) => item.id !== booklist.id))
-
-        navigate('/bookdetails')
-    }
 
     const searchFilteredData = booklist.filter((item) => item.title.toLowerCase().includes(search.toLowerCase()));
 
     return (
-
         <div className='flex flex-col justify-center items-center bg-blue-100 '>
 
             <label htmlFor="title" className='flex gap-5 items-center my-2'>
                 <input type="search" className='p-2 border ' id='search' value={search} onChange={(e) => setSearch(e.target.value)} placeholder='Search' />
             </label>
-
-           
-
-            {/* {
-                booklist.filter((item) => item.title === search)
-            } */}
-            
-
             {
                 booklist.length == 0 ? <div className='text-4xl flex justify-center items-center h-[calc(100svh-65px)] bg-blue-100 w-screen text-red-600'>No Books Available</div> : searchFilteredData.map((details) => {
                     return (
@@ -72,13 +54,17 @@ const BookCard = () => {
                                                 <button className='bg-red-600 text-white rounded hover:bg-red-100 hover:text-red-700 border hover:border-red-700 py-1 px-3' onClick={() => { setBook(booklist.filter((item) => item.id !== details.id)) }}>Delete</button>
 
                                             </div>
-
                                         </div>
                                     </li>
                                 </ul>
 
                                 <div>
-                                    <button className='text-center bg-blue-600 text-white py-1 px-4 mt-5 rounded hover:text-blue-600 border hover:border-blue-600 hover:bg-blue-100' onClick={set_BookDetails}>Read More</button>
+                                    <button className='text-center bg-blue-600 text-white py-1 px-4 mt-5 rounded hover:text-blue-600 border hover:border-blue-600 hover:bg-blue-100' onClick={() => {
+                                        setBook(booklist.filter((item) => item.id === details.id))
+                                        navigate('/bookdetails')
+                                    }}>
+                                        Read More
+                                    </button>
                                 </div>
                             </div>
 
@@ -86,7 +72,6 @@ const BookCard = () => {
                     )
                 })
             }
-
 
         </div>
     )
--- Revision/Task_/E_library_management_system/src/Pages/BookDetails.jsx
+++ Revision/Task_/E_library_management_system/src/Pages/BookDetails.jsx
@@ -21,39 +21,40 @@ const BookDetails = () => {
                     booklist.map((details) => {
                         return (
                             <div key={details.id} className='flex justify-center items-center '>
-                                
-                                <div className="flex-1  min-w-0  p-3 *:text-bold gap-3 border   mt-10">
-                                    <p className=" font-medium text-gray-900 truncate ">
-                                        Title :
-                                        <span className='text-gray-500'>
-                                            {details.title}
-                                        </span>
-                                    </p>
+                                {
+                                    booklist.filter((item) => item.id !== details.id) ? <div className="flex-1  min-w-0  p-3 *:text-bold gap-3 border   mt-10">
+                                        <p className=" font-medium text-gray-900 truncate ">
+                                            Title :
+                                            <span className='text-gray-500'>
+                                                {details.title}
+                                            </span>
+                                        </p>
 
-                                    <p className=" text-gray-900 truncate ">
-                                        Author :
-                                        <span className='text-gray-500'>
-                                            {details.author}
-                                        </span>
-                                    </p>
-                                    
-                                    <p className=" text-gray-900 truncate ">
-                                        Status:
-                                        <span className='text-gray-500'>
-                                            {details.status}
-                                        </span>
-                                    </p>
-                                </div>
+                                        <p className=" text-gray-900 truncate ">
+                                            Author :
+                                            <span className='text-gray-500'>
+                                                {details.author}
+                                            </span>
+                                        </p>
+
+                                        <p className=" text-gray-900 truncate ">
+                                            Status:
+                                            <span className='text-gray-500'>
+                                                {details.status}
+                                            </span>
+                                        </p>
+                                    </div> : ''
+                                }
                             </div>
                         )
                     })
                 }
-                </div>
-                <div>
-
-                    <button className='text-center bg-blue-600 text-white py-1 px-4 mt-5 rounded hover:text-blue-600 border hover:border-blue-600 hover:bg-blue-100' onClick={() => navigate('/bookcard')}>Back</button>
             </div>
-        </div>
+            <div>
+
+                <button className='text-center bg-blue-600 text-white py-1 px-4 mt-5 rounded hover:text-blue-600 border hover:border-blue-600 hover:bg-blue-100' onClick={() => navigate('/bookcard')}>Back</button>
+            </div>
+        </div >
     )
 }
 
