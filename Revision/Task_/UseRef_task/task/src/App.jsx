724d150ace972ae462a29f4f19f6017de933581c
Chetan <Chetan.sq05@gmail.com>
2025-01-17 12:01:11 +0530

    scroll img task continue... 17-01-25

--- Revision/Task_/UseRef_task/task/src/App.jsx
+++ Revision/Task_/UseRef_task/task/src/App.jsx
@@ -1,3 +1,6 @@
+import Click_counter from "./Click_counter"
+import Focus from "./Focus"
+import Scroll_image from "./Scroll_image"
 import Play_pause from "./play_pause_video/Play_pause"
 import Stopwatch from "./stopwatch/Stopwatch"
 
@@ -7,7 +10,13 @@ function App() {
     <>
       {/* <Stopwatch /> */}
 
-      <Play_pause />
+      {/* <Play_pause /> */}
+
+      {/* <Focus /> */}
+
+      {/* <Click_counter /> */}
+
+      <Scroll_image />
     </>
   )
 }
--- Revision/Task_/UseRef_task/task/src/Focus.jsx
+++ Revision/Task_/UseRef_task/task/src/Focus.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Focus = () => {
 
+    const ref = useRef(null)
+
+    const focus_input = () => {
+        ref.current.focus()
+    }
     return (
-        <div>
+        <div className='flex justify-center items-center h-screen bg-slate-500 gap-4'>
 
-            <input type="text" />
+            <input type="text" ref={ref} className='px-2 py-1' />
 
+            <button onClick={focus_input} className='bg-slate-900 text-white py-1 px-2 hover:bg-slate-800'> Focus on input</button>
         </div>
     )
 }
