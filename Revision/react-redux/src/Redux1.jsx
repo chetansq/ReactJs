d3460535d815db4e89c828c07e43cda394ae9cc9
chetan <chetan.sq05@gmail.com>
2025-02-08 17:14:28 +0530

    changes

--- Revision/react-redux/src/Redux1.jsx
+++ Revision/react-redux/src/Redux1.jsx
@@ -14,7 +14,8 @@ const Redux = () => {
     const dispatch = useDispatch();
 
     return (
-        <div>
+        <div className='flex justify-center items-center h-screen bg-gray-100'>
+
             <button className='btn mx-1' onClick={() => dispatch(Cake())}>Cake</button>
             <button className='btn mx-1' onClick={() => dispatch(Choco())}>Choco</button>
         </div>
--- Revision/react-redux/src/main.jsx
+++ Revision/react-redux/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { Store } from '../Store.jsx'
+import { Store } from './Store.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={Store}>
