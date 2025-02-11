3cac576cb4187d9a952a807de61297f970f65b19
chetan <chetan.sq05@gmail.com>
2025-02-11 17:17:40 +0530

    shop cart task continue

---
+++ Revision/Task_/shopping_cart/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Shop from './Shop'
+
+function App() {
+
+  return (
+    <>
+      <Shop />
+    </>
+  )
+}
+
+export default App
---
+++ Revision/Task_/shopping_cart/src/Reducer.jsx
@@ -0,0 +1,26 @@
+import { add_item } from "./Action";
+import { ADD_ITEM } from "./Constant";
+
+const initialvalue1 = 30
+const initialvalue2 = 20
+
+export const addReducer1 = (state = initialvalue1, action) => {
+    switch (action.type) {
+        case ADD_ITEM:
+            return state - 1
+            break;
+
+        default: return state
+            break;
+    }
+}
+export const addReducer2 = (state = initialvalue1, action) => {
+    switch (action.type) {
+        case ADD_ITEM:
+            return state - 1
+            break;
+
+        default: return state
+            break;
+    }
+}
