df2f3134c0c9c302e470b743a0352933cfeb37b6
chetan <chetan.sq05@gmail.com>
2025-02-17 18:06:38 +0530

    axios baki

---
+++ Revision/axios/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GetRequest from './axios/GetRequest'
+
+function App() {
+
+  return (
+    <>
+      <GetRequest />
+    </>
+  )
+}
+
+export default App
---
+++ Revision/axios/src/axios/GetRequest.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const GetRequest = () => {
+
+    const [data, setData] = useState([])
+    console.log(data);
+    const handleData = () => { 
+        
+    }
+    return (
+        <div>GetRequest</div>
+    )
+}
+
+export default GetRequest
